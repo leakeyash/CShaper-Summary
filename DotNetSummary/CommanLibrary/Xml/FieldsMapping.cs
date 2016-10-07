eda193bba1d8049721bbda9d940731732c606250
leakeyash <123661285@qq.com>
2016-10-07 20:32:29 +0800

    update

---
+++ DotNetSummary/CommanLibrary/Xml/FieldsMapping.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommanLibrary.Xml
+{   
+    public class FieldsMapping
+    {
+        readonly XmlDocument _xml=new XmlDocument();
+        private readonly bool _loaded = false;
+        public FieldsMapping(string xmlPath)
+        {
+            if (!string.IsNullOrEmpty(xmlPath) && File.Exists(xmlPath))
+            {
+                _xml.Load(xmlPath);
+                _loaded = true;
+            }
+        }
+
+        private Dictionary<string, string> GetSpecificNodeDictionary(string xPath)
+        {
+            Dictionary<string,string> selectedValues = new Dictionary<string, string>();
+            if (!_loaded || string.IsNullOrEmpty(xPath)) return selectedValues;
+            XmlNodeList nodeList = _xml.SelectNodes(xPath);
+            if (nodeList == null) return null;
+            foreach (XmlNode node in nodeList)
+            {
+                if (node.Attributes != null)
+                {
+                    string name = string.Empty;
+                    string value = string.Empty;
+                    foreach (XmlAttribute xmlAttribute in node.Attributes)
+                    {
+                        if (xmlAttribute.Name == "name")
+                        {
+                            name = xmlAttribute.Value;
+                        }
+                        if (xmlAttribute.Name == "value")
+                        {
+                            value = xmlAttribute.Value;
+                        }
+                    }
+                    if (!string.IsNullOrEmpty(name) && !selectedValues.ContainsKey(name))
+                    {
+                        selectedValues.Add(name, value);
+                    }
+                }
+            }
+            return selectedValues;
+        }  
+        public string ReplaceSpecificNodes(string text, string xPath,string symbolStart,string symbolEnd,out Dictionary<string,string> selectedValues,out List<string> unMappingList)
+        {          
+            unMappingList=new List<string>();
+            string replacedString = text;
+
+            selectedValues = GetSpecificNodeDictionary(xPath);
+
+            replacedString = ReplacedString(text, symbolStart, symbolEnd, selectedValues, unMappingList, replacedString);
+
+            return replacedString;
+        }
+
+        private static string ReplacedString(string text, string symbolStart, string symbolEnd, Dictionary<string, string> selectedValues,
+            List<string> unMappingList, string replacedString)
+        {
+            int tempStart = text.IndexOf(symbolStart, StringComparison.OrdinalIgnoreCase);
+            while (tempStart > 0)
+            {
+                int tempEnd = text.IndexOf(symbolEnd, tempStart + symbolStart.Length, StringComparison.OrdinalIgnoreCase);
+                if (tempEnd > 0)
+                {
+                    string mapString = text.Substring(tempStart + symbolStart.Length,
+                        tempEnd + symbolEnd.Length - tempStart - symbolStart.Length - 1);
+                    string wholeString = text.Substring(tempStart, tempEnd + symbolEnd.Length - tempStart);
+                    if (selectedValues.ContainsKey(mapString))
+                    {
+                        replacedString = replacedString.Replace(wholeString, selectedValues[mapString]);
+                    }
+                    else
+                    {
+                        unMappingList.Add(wholeString);
+                    }
+                    tempStart = replacedString.IndexOf(symbolStart, tempEnd + symbolEnd.Length,
+                        StringComparison.OrdinalIgnoreCase);
+                }
+                tempStart = replacedString.IndexOf(symbolStart, tempStart + symbolStart.Length,
+                    StringComparison.OrdinalIgnoreCase);
+            }
+            return replacedString;
+        }
+    }
+}
---
+++ DotNetSummary/Tools/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tools
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            CommanLibrary.Xml.FieldsMapping fm = new CommanLibrary.Xml.FieldsMapping(TextBoxFilePath.Text);
+            Dictionary<string, string> selectedDictionary;
+            List<string> missList;
+            TextBoxReplaced.Text = fm.ReplaceSpecificNodes(TextBoxOrigin.Text, TextBoxXpath.Text, TextBoxSymbolStart.Text,
+                TextBoxSymbolEnd.Text, out selectedDictionary, out missList);
+            TextBoxMisMatchs.Text = missList.Aggregate(string.Empty, (current, s) => current + (s + "\r\n"));
+        }
+    }
+}
